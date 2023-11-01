809bbf8b796a97c91eb6e124e237a0b843003235
KAYOKG <kayo.kg@hotmail.com>
2023-11-01 13:54:56 -0300

    up

--- commands/ponto.js
+++ commands/ponto.js
@@ -2,43 +2,42 @@
 const Discord = require("discord.js");
 const config = require('../config.json')
 
-// const emoji = {
-//     setas: config.emojis.emojiSetas,
-//     noverify: config.emojis.emojiNoVerify,
-//     verify: config.emojis.emojiVerify,
-//     };
+const emoji = {
+    setas: config.emojis.emojiSetas,
+    noverify: config.emojis.emojiNoVerify,
+    verify: config.emojis.emojiVerify,
+    };
 
-module.exports = {
-  run: async (client, message, args) => {
+    module.exports = {
+    run: async(client, message, args) => {
     message.delete();
-    if (!message.member.permissions.has("ADMINISTATOR")) return message.reply("**Desculpa mas você não tem permissão de \`Funcionario\`!")
+    if(!message.member.permissions.has("ADMINISTATOR")) return message.reply("**Desculpa mas você não tem permissão de \`Funcionario\`!")
+ 
 
+                            
+                const embed = new Discord.MessageEmbed()
+                .setDescription(`**Bot System - Controle de Ponto** \n\n${emoji.setas} Para **Iniciar** seu **Ponto** clique no botão ${emoji.verify} **Abrir Ponto**\n\n${emoji.setas} Para **Finalizar** seu **Ponto** clique no botão ${emoji.noverify} **Fechar Ponto** `)
+                .setFooter({ text: 'By Venidici', iconURL: message.guild.iconURL({format: "png"}) });
+            
+                
+               
+                let buttonsStock = new Discord.MessageActionRow()
+                .addComponents(
+                    new Discord.MessageButton()
+                        .setCustomId('iniciar-ponto')
+                        .setStyle('SUCCESS')
+                        .setLabel('Iniciar')
+                        .setEmoji(`${emoji.verify}`)
+                        .setDisabled(false),
+                        new Discord.MessageButton()
+                            .setCustomId('finalizar-ponto')
+                            .setStyle('DANGER')
+                            .setLabel('Finalizar')
+                            .setEmoji(`${emoji.noverify}`)
+                            .setDisabled(false),
+                )
 
+                message.channel.send({ embeds: [embed], components: [buttonsStock]})
 
-    const embed = new Discord.MessageEmbed()
-      .setDescription(`**Bot - Controle de Ponto** \n\n Para **Iniciar** seu **Ponto** clique no botão  **Abrir Ponto**\n\n Para **Finalizar** seu **Ponto** clique no botão **Fechar Ponto** `)
-      .setFooter({ text: 'By Venidici', iconURL: message.guild.iconURL({ format: "png" }) });
-
-
-
-    let buttonsStock = new Discord.MessageActionRow()
-      .addComponents(
-        new Discord.MessageButton()
-          .setCustomId('iniciar-ponto')
-          .setStyle('SUCCESS')
-          .setLabel('Iniciar Ponto')
-          // .setEmoji(`${emoji.verify}`)
-          .setDisabled(false),
-        new Discord.MessageButton()
-          .setCustomId('finalizar-ponto')
-          .setStyle('DANGER')
-          .setLabel('Finalizar Ponto')
-          // .setEmoji(`${emoji.noverify}`)
-          .setDisabled(false),
-      )
-
-    message.channel.send({ embeds: [embed], components: [buttonsStock] })
-
-
-  }
-}
+       
+}}
